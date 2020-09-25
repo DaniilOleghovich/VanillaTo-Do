8533b5dd97c92cd0eb08336218c3dd9627725b98
DaniilOleghovich <danya.pavlichenko.1997@mail.ru>
2020-09-25 10:24:15 +0300

    create layout and get input, button and list in js

--- js/script.js
+++ js/script.js
@@ -3,3 +3,5 @@ const buttonTodo = document.querySelector('.todo-btn');
 const listTodo = document.querySelector('.todo-list');
 
 // console.log(inputTodo, buttonTodo, listTodo);
+// console.log(inputTodo, buttonTodo, listTodo);
+// console.log(inputTodo, buttonTodo, listTodo);
