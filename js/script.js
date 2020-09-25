f0a61efcc75015df59118e59261560dc27e490bf
DaniilOleghovich <danya.pavlichenko.1997@mail.ru>
2020-09-25 09:48:29 +0300

    initial commit

---
+++ js/script.js
@@ -0,0 +1,5 @@
+const inputTodo = document.querySelector('.todo-input');
+const buttonTodo = document.querySelector('.todo-btn');
+const listTodo = document.querySelector('.todo-list');
+
+// console.log(inputTodo, buttonTodo, listTodo);
